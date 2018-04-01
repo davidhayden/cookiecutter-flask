825e4be2b2c4f540b45dd8bda5a7132e23ac94c2
davidhayden <davidhayden@users.noreply.github.com>
2018-04-01 13:12:06 -0400

    Speciying not nullable on User columns.

--- {{cookiecutter.app_name}}/app/models.py
+++ {{cookiecutter.app_name}}/app/models.py
@@ -8,9 +8,9 @@ class User(UserMixin, db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    username = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
 
 
     def __init__(self, username, email, password=None, **kwargs):
